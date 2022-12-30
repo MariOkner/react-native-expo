4cdaced9b6e9d5f8a741161154999376a5f6c2cb
Marina <mariokner@gmail.com>
2022-12-30 22:20:45 +0200

    Debugger

--- myNewProject/App.js
+++ myNewProject/App.js
@@ -1,151 +1,55 @@
-import React, { useState } from "react";
-// import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  View,
-  ImageBackground,
-  TextInput,
-  TouchableOpacity,
-  Platform,
-  KeyboardAvoidingView,
-  Keyboard,
-  TouchableWithoutFeedback,
-} from "react-native";
+{
+  /* <script src="http://localhost:19000"></script>; */
+}
+import React, { useCallback, useEffect, useState } from "react";
+// import { globalStyle } from "./styles/style";
+
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
+import { View } from "react-native";
+
+import LoginScreen from "./screens/LoginScreen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  console.log(Platform.OS);
-  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+  const [appIsReady, setAppIsReady] = useState(false);
 
-  const keyboardHide = () => {
-    setIsShowKeyboard(false);
-    Keyboard.dismiss();
-  };
+  // Fonts
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync({
+          "andika-r": require("./assets/fonts/Andika-Regular.ttf"),
+          "andika-b": require("./assets/fonts/Andika-Bold.ttf"),
+          "Rubik-Bold": require("./assets/fonts/Rubik-Bold.ttf"),
+        });
+      } catch (error) {
+        console.log(message.error);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+  //__________________________________________________________________________
   return (
-    <TouchableWithoutFeedback onPress={keyboardHide}>
-      <View style={styles.container}>
-        <ImageBackground
-          style={styles.image}
-          source={require("./assets/images/background1.jpg")}
-        >
-          <KeyboardAvoidingView
-            behavior={Platform.OS === "ios" ? "padding" : "height"}
-          >
-            <View
-              style={{ ...styles.form, marginBottom: isShowKeyboard ? 20 : 20 }}
-            >
-              <View style={styles.header}>
-                <Text style={styles.headerTitle}>Регистрация</Text>
-              </View>
-              <View>
-                <Text style={styles.inputTitle}>Email</Text>
-                <TextInput
-                  style={styles.input}
-                  onFocus={() => setIsShowKeyboard(true)}
-                />
-              </View>
-              <View style={{ marginTop: 20 }}>
-                <Text style={styles.inputTitle}>Password</Text>
-                <TextInput
-                  style={styles.input}
-                  secureTextEntry={true}
-                  onFocus={() => setIsShowKeyboard(true)}
-                />
-              </View>
-              <TouchableOpacity
-                activeOpacity={0.6}
-                style={styles.button}
-                onPress={keyboardHide}
-              >
-                <Text style={styles.btnTitle}>SIGN IN</Text>
-              </TouchableOpacity>
-            </View>
-
-            {/* <Text style={styles.text}>Hello, friend!</Text> */}
-          </KeyboardAvoidingView>
-        </ImageBackground>
-
-        {/* <StatusBar style="auto" /> */}
-      </View>
-    </TouchableWithoutFeedback>
+    <View
+      onLayout={onLayoutRootView}
+      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+    >
+      <LoginScreen />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#000",
-    // justifyContent: "center",
-  },
-  image: {
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center",
-    // justifyContent: "flex-end",
-    // alignItems: "center",
-  },
-  form: {
-    marginHorizontal: 50,
-  },
-  header: {
-    alignItems: "center",
-    marginBottom: 150,
-  },
-  headerTitle: {
-    color: "#fff",
-    // backgroundColor: "#f0f8ff",
-    fontSize: 30,
-    // borderRadius: 6,
-    // paddingVertical: 5,
-    // paddingHorizontal: 15,
-  },
-  inputTitle: {
-    marginBottom: 2,
-    fontSize: 16,
-  },
-  input: {
-    borderWidth: 2,
-    borderColor: "",
-    height: 40,
-    borderRadius: 6,
-
-    color: "#000000",
-    backgroundColor: "#f0f8ff",
-    opacity: 0.6,
-    paddingLeft: 10,
-  },
-  button: {
-    marginTop: 20,
-    height: 40,
-    borderRadius: 6,
-    marginHorizontal: 40,
-    justifyContent: "center",
-    alignItems: "center",
-    ...Platform.select({
-      ios: {
-        backgroundColor: "transparent",
-        borderColor: "#ff8c00",
-      },
-      android: {
-        backgroundColor: "#ff8c00",
-        borderColor: "transparent",
-      },
-    }),
-  },
-  btnTitle: {
-    fontSize: 18,
-    fontWeight: "900",
-    ...Platform.select({
-      ios: {
-        color: "#ff8c00",
-      },
-      android: {
-        color: "#fff",
-      },
-    }),
-  },
-  text: {
-    color: "pink",
-    fontSize: 30,
-  },
-});
---
+++ myNewProject/styles/style.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+    // padding: 20,
+    backgroundColor: "#000",
+  },
+  title: {
+    fontSize: 20,
+    color: "#252525",
+    fontFamily: "andika-b",
+  },
+});
