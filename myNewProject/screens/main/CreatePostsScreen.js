22ea537ed761fcea878275101b97102ac4a279dc
Marina <mariokner@gmail.com>
2023-01-05 01:14:45 +0200

    camera added

--- myNewProject/screens/main/CreatePostsScreen.js
+++ myNewProject/screens/main/CreatePostsScreen.js
@@ -1,12 +1,70 @@
-import React from "react";
-import { Camera } from "expo-camera";
+import React, { useState, useRef, useEffect } from "react";
+import { Camera, CameraType } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
 
-import { Text, StyleSheet, View } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import { Text, StyleSheet, View, TouchableOpacity } from "react-native";
+
+import { useIsFocused } from "@react-navigation/native";
 
 const CreatePostsScreen = () => {
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  // _________
+  const [image, setImage] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
+  const cameraRef = useRef(null);
+  const focused = useIsFocused();
+
+  useEffect(() => {
+    (async () => {
+      MediaLibrary.requestPermissionsAsync();
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === "granted");
+    })();
+  }, []);
+
+  //  useFocusEffect(() => {
+  //    (async () => {
+  //      MediaLibrary.requestPermissionsAsync();
+  //      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+  //      console.log(cameraStatus);
+  //      setHasCameraPermission(cameraStatus.status === "granted");
+  //    })();
+  //  });
+  // ___________
+
+  const takePhoto = async () => {
+    if (useRef) {
+      try {
+        const data = await cameraRef.current.takePictureAsync();
+        console.log(data);
+        setImage(data.uri);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  if (hasCameraPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Camera style={styles.camera}></Camera>
+      {focused && (
+        <Camera
+          style={styles.camera}
+          type={type}
+          flashMode={flash}
+          ref={cameraRef}
+        >
+          <TouchableOpacity style={styles.button} onPress={takePhoto}>
+            <MaterialCommunityIcons name="camera" size={32} color="#808080" />
+          </TouchableOpacity>
+        </Camera>
+      )}
     </View>
   );
 };
@@ -16,8 +74,16 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   camera: {
-    marginTop: 50,
+    // marginTop: 50,
     height: 300,
+    justifyContent: "center",
+    alignItems: "center",
+    color: "#E8E8E8",
+  },
+  button: {
+    padding: 10,
+    backgroundColor: "rgba(255, 255, 255, 0.8)",
+    borderRadius: 50,
   },
 });
 
--- myNewProject/screens/main/PostsScreen.js
+++ myNewProject/screens/main/PostsScreen.js
@@ -1,11 +1,24 @@
 import React from "react";
 
+import { AntDesign } from "@expo/vector-icons";
+
 import { Text, StyleSheet, View } from "react-native";
 
 const PostsScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>PostsScreen</Text>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Публікації</Text>
+        <AntDesign
+          style={styles.logout}
+          name="logout"
+          size={24}
+          color="black"
+        />
+      </View>
+      <View style={styles.mainBox}>
+        <Text>PostsScreen</Text>
+      </View>
     </View>
   );
 };
@@ -13,6 +26,30 @@ const PostsScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+
+    // justifyContent: "center",
+    // alignItems: "center",
+  },
+  header: {
+    flex: 1,
+    paddingHorizontal: 15,
+    marginTop: 30,
+    flexDirection: "row",
+    borderColor: "#a9a9a9",
+    borderBottomWidth: 2,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  headerText: {
+    fontSize: 20,
+    fontFamily: "Rubik-Bold",
+    marginRight: 100,
+  },
+  logout: {
+    // justifyContent: "flex-end",
+  },
+  mainBox: {
+    flex: 8,
     justifyContent: "center",
     alignItems: "center",
   },
